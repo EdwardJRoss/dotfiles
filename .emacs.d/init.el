f566cf5c55b0f6739adb4e46859059502a436278
Edward Ross <edward@skeptric.com>
2018-02-15 16:23:03 +1100

    Emacs: add yasnippet

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -908,3 +908,17 @@ Lisp function does not specify a special indentation."
   :config
   (global-set-key (kbd "C-c u") 'caps-lock-mode)
   (evil-define-key 'normal 'global "zu" 'caps-lock-mode))
+
+
+;; TODO: Version snippets
+;; TODO: Local snippets
+(use-package yasnippet
+  :config
+  (defun yas--downcase-key (args) ; args = (table key)
+    (cl-callf downcase (nth 1 args))
+    args)
+
+  ;; Make snippet keys case insensitive
+  (advice-add 'yas--fetch :filter-args #'yas--downcase-key)
+
+  (yas-global-mode 1))

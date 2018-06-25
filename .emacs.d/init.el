66fb8c29d012dd1ebc525d15cea8c3ff218fa81b
Edward Ross <edward@skeptric.com>
2018-06-25 14:57:32 +1000

    Emacs: add rename buffer and file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1000,6 +1000,22 @@ Lisp function does not specify a special indentation."
 
   (yas-global-mode 1))
 
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
 
 ;; Put/save customisations through customize in a separate file
 (defconst custom-file (expand-file-name "custom.el" user-emacs-directory))
